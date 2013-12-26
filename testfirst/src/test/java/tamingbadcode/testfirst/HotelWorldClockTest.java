9b00c45b3fd568a246f437c9b670368c8a6a3a8b
Ben Wu <wubin28@gmail.com>
2013-12-26 18:33:14 +0800

    Added test class HotelWorldClockTest.

---
+++ testfirst/src/test/java/tamingbadcode/testfirst/HotelWorldClockTest.java
@@ -0,0 +1,11 @@
+package tamingbadcode.testfirst;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-26
+ * Time: 下午6:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class HotelWorldClockTest {
+}
