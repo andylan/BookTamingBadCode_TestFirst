649c8e563c851b422b4849990fa8d69d8d74814c
Ben Wu <wubin28@gmail.com>
2013-12-26 21:07:30 +0800

    Added test WHEN_BeijingIsSetToNineOclock().

--- testfirst/src/test/java/tamingbadcode/testfirst/HotelWorldClockTest.java
+++ testfirst/src/test/java/tamingbadcode/testfirst/HotelWorldClockTest.java
@@ -1,5 +1,9 @@
 package tamingbadcode.testfirst;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Ben
@@ -7,5 +11,21 @@ package tamingbadcode.testfirst;
  * Time: 下午6:30
  * To change this template use File | Settings | File Templates.
  */
+@RunWith(JUnit4.class)
 public class HotelWorldClockTest {
+    @Test
+    public void WHEN_BeijingIsSetToNineOclock() {
+        // Given
+        CityClock beijingClock = new BeijingClock(8);
+        CityClock londonClock = new LondonClock(0);
+        CityClock moscowClock = new MoscowClock(4);
+        CityClock sydneyClock = new SydneyClock(10);
+        CityClock newYorkClock = new NewYorkClock(-5);
+
+        // When
+        beijingClock.setLocalTime(9);
+
+        // Then
+        assertEquals("Failure - the local time of London should be 1", 1, londonClock.getLocalTime());
+    }
 }
