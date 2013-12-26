56cb68408e70ca111498a07405b6daeb8af81aa2
Ben Wu <wubin28@gmail.com>
2013-12-26 21:13:03 +0800

    Added class LondonClock, MoscowClock, NewYorkClock and SydneyClock.

---
+++ testfirst/src/main/java/tamingbadcode/testfirst/MoscowClock.java
@@ -0,0 +1,14 @@
+package tamingbadcode.testfirst;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-26
+ * Time: 下午9:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class MoscowClock extends CityClock {
+    public MoscowClock(int localTime) {
+        super(localTime);
+    }
+}
