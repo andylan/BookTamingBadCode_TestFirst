67b12758c0faa3e6776407b72b5783b2d46422a4
Ben Wu <wubin28@gmail.com>
2013-12-26 21:10:53 +0800

    Added class CityClock and BeijingClock.

---
+++ testfirst/src/main/java/tamingbadcode/testfirst/BeijingClock.java
@@ -0,0 +1,14 @@
+package tamingbadcode.testfirst;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-26
+ * Time: 下午9:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class BeijingClock extends CityClock {
+    public BeijingClock(int localTime) {
+        super(localTime);
+    }
+}
---
+++ testfirst/src/main/java/tamingbadcode/testfirst/CityClock.java
@@ -0,0 +1,16 @@
+package tamingbadcode.testfirst;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-26
+ * Time: 下午9:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class CityClock {
+    private int localTime;
+
+    public CityClock(int localTime) {
+        this.localTime = localTime;
+    }
+}
