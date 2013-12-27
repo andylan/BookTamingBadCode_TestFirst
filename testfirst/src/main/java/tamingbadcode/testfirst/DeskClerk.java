6eee2211f503dcb6b8e1dabc248f3ce9177823da
Ben Wu <wubin28@gmail.com>
2013-12-27 13:02:38 +0800

    Created class DeskClerk.

---
+++ testfirst/src/main/java/tamingbadcode/testfirst/DeskClerk.java
@@ -0,0 +1,11 @@
+package tamingbadcode.testfirst;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-27
+ * Time: 下午1:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeskClerk {
+}
