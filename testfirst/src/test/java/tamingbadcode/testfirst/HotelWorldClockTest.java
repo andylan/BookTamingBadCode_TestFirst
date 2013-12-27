d5416188aadf7c5c4b0a3114ff4f595d2dc310c8
Ben Wu <wubin28@gmail.com>
2013-12-27 12:59:03 +0800

    Updated the test WHEN_BeijingIsSetToNineOclock() with DeskClerk class.

--- testfirst/src/test/java/tamingbadcode/testfirst/HotelWorldClockTest.java
+++ testfirst/src/test/java/tamingbadcode/testfirst/HotelWorldClockTest.java
@@ -18,20 +18,25 @@ public class HotelWorldClockTest {
     @Test
     public void WHEN_BeijingIsSetToNineOclock() {
         // Given
-        CityClock beijingClock = new BeijingClock(8);
-        CityClock londonClock = new LondonClock(0);
-        CityClock moscowClock = new MoscowClock(4);
-        CityClock sydneyClock = new SydneyClock(10);
-        CityClock newYorkClock = new NewYorkClock(-5);
+        deskClerk.addCityClock("Beijing", new BeijingClock(8));
+        deskClerk.addCityClock("London", new LondonClock(0));
+        deskClerk.addCityClock("Moscow", new MoscowClock(4));
+        deskClerk.addCityClock("Sydney", new SydneyClock(10));
+        deskClerk.addCityClock("NewYork", new NewYorkClock(-5));
 
         // When
-        beijingClock.setLocalTime(9);
+        deskClerk.setLocalTimeToCityClock(9, "Beijing");
 
         // Then
-        assertEquals("Failure - the local time of Beijing should be 9", 9, beijingClock.getLocalTime());
-        assertEquals("Failure - the local time of London should be 1", 1, londonClock.getLocalTime());
-        assertEquals("Failure - the local time of Moscow should be 5", 5, moscowClock.getLocalTime());
-        assertEquals("Failure - the local time of Sydney should be 11", 11, sydneyClock.getLocalTime());
-        assertEquals("Failure - the local time of New York should be 20", 20, newYorkClock.getLocalTime());
+        assertEquals("Failure - the local time of Beijing should be 9"
+                , 9, deskClerk.getCityClock("Beijing").getLocalTime());
+        assertEquals("Failure - the local time of London should be 1"
+                , 1, deskClerk.getCityClock("London").getLocalTime());
+        assertEquals("Failure - the local time of Moscow should be 5"
+                , 5, deskClerk.getCityClock("Moscow").getLocalTime());
+        assertEquals("Failure - the local time of Sydney should be 11"
+                , 11, deskClerk.getCityClock("Sydney").getLocalTime());
+        assertEquals("Failure - the local time of New York should be 20"
+                , 20, deskClerk.getCityClock("NewYork").getLocalTime());
     }
 }
